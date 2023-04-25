a64a34565226eb0ff0fc32535e1b5d6acc9dbe6e
Ibrahim Khan <aitvetibrahim@gmail.com>
2023-04-25 13:06:59 +0600

    day work done

--- lib/pushnotification/fcm_utils.dart
+++ lib/pushnotification/fcm_utils.dart
@@ -13,13 +13,28 @@ class FCMUtils {
         criticalAlert: false,
         provisional: false,
         sound: true);
-    // FirebaseMessaging.instance.getInitialMessage();
+    RemoteMessage? message =
+        await FirebaseMessaging.instance.getInitialMessage();
+    if (message != null) {
+      messageHandler(message);
+    }
     FirebaseMessaging.onMessage.listen(messageHandler);
     FirebaseMessaging.onBackgroundMessage(messageHandler);
+    getToken();
+    listenToTopic();
   }
 
   Future<void> messageHandler(RemoteMessage message) async {
     log(message.notification?.title ?? 'Null title');
     log(message.notification?.body ?? 'Null body');
+  }
+  
+  Future<void> getToken()async{
+    final String? token =  await _firebaseMessaging.getToken();
+    log(token ?? '');
+  }
+
+  Future<void> listenToTopic()async{
+    await _firebaseMessaging.subscribeToTopic('ibrahim');
   }
 }
